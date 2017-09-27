2ccd4de447de8da23a90745aa0e68717644ffa0a
天 谢 <jsjtxietian@outlook.com>
2017-09-28 00:31:45 +0800

    2.3.1结束

--- define-pic.rkt
+++ define-pic.rkt
@@ -1,2 +1,5 @@
 #lang sicp
-(#%require sicp-pict)
+;(#%require sicp-pict)
+(#%require graphics/graphics)
+(open-graphics)
+(#%require ( planet "sicp.ss" ( "soegaard" "sicp.plt" 2 1)))
--- define.rkt
+++ define.rkt
@@ -1,5 +1,2 @@
 #lang planet neil/sicp
-;(#%require sicp-pict)
-(#%require graphics/graphics)
-(open-graphics)
-(#%require ( planet "sicp.ss" ( "soegaard" "sicp.plt" 2 1)))
+
