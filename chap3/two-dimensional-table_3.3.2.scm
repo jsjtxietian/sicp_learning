0edaf70acfbc168e77bf700c5f521b2b6d6dc065
天 谢 <jsjtxietian@outlook.com>
2017-11-11 14:04:32 +0800

    3.3.2书本代码ok

---
+++ chap3/two-dimensional-table_3.3.2.scm
@@ -0,0 +1,24 @@
+(load "C:\\Users\\jsjtx\\Desktop\\sicp_learning\\chap3\\one-dimensional-table_3.3.2.scm")
+
+(define (lookup key-1 key-2 table)
+    (let ((subtable (assoc key-1 (cdr table))))
+        (if subtable
+            (let ((record (assoc key-2 (cdr subtable))))
+                (if record
+                    (cdr record)
+                    #f))
+            #f
+        )
+    )
+)
+
+(define (insert! key-1 key-2 value table)
+    (let ((subtable (assoc key-1 (cdr table))))
+        (if subtable
+            (let ((record (assoc key-2 (cdr subtable))))
+                (if record
+                    (set-cdr! record value)
+                    (set-cdr! subtable (cons (cons key-2 value) (cdr subtable)))))   
+            (set-cdr! table (cons (list key-1 (cons key-2 value)) (cdr table)))))
+    'ok
+)
