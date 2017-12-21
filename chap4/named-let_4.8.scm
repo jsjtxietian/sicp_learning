b55a2398d736a972bcacb3c535568fa4bef92952
天 谢 <jsjtxietian@outlook.com>
2017-12-21 19:26:46 +0800

    4.8,4.5未完成、

---
+++ chap4/named-let_4.8.scm
@@ -0,0 +1,44 @@
+(load "let_4.6.scm")
+
+(define (named-let? expr) 
+    (and 
+        (let? expr) 
+        (symbol? (cadr expr)))) 
+
+(define (named-let-func-name expr) 
+    (cadr expr)) 
+
+(define (named-let-func-body expr) 
+    (cadddr expr)) 
+
+(define (named-let-func-parameters expr) 
+    (map car (caddr expr))) 
+
+(define (named-let-func-inits expr) 
+    (map cadr (caddr expr))) 
+
+(define (named-let->func expr) 
+    (list 'define  
+        (cons (named-let-func-name expr) (named-let-func-parameters expr)) 
+        (named-let-func-body expr))) 
+
+(define (let->combination expr) 
+    (if (named-let? expr) 
+        (sequence->exp 
+            (list (named-let->func expr) 
+                    (cons (named-let-func-name expr) (named-let-func-inits expr)))) 
+        (cons 
+            (make-lambda (let-vars expr) 
+                (list (let-body expr))) 
+            (let-inits expr)))) 
+
+;;; (define (fib n)
+;;;     (let 
+;;;         fib-iter
+;;;         ((a 1) (b 0) (count n))
+;;;         (if (= count 0)
+;;;             b
+;;;             (fib-iter (+ a b) a (- count 1)))))
+
+(define the-global-environment (setup-environment))
+; (driver-loop)
